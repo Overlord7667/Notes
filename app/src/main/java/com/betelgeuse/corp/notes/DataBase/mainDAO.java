d6a9683ceda639b783aba2289a0e71fb5bef16e5
Overlord7667 <aspiaro@gmail.com>
2023-10-03 21:21:31 +0300

    Initial commit

---
+++ app/src/main/java/com/betelgeuse/corp/notes/DataBase/mainDAO.java
@@ -0,0 +1,26 @@
+package com.betelgeuse.corp.notes.DataBase;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import com.betelgeuse.corp.notes.Models.Notes;
+
+import java.util.List;
+
+@Dao
+public interface mainDAO {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insert (Notes notes);
+
+    @Query ("SELECT * FROM notes ORDER BY id DESC")
+    List<Notes> getAll();
+
+    @Query ("UPDATE notes SET title = :title, notes = :notes WHERE ID = :id")
+    void update(int id, String title, String notes);
+
+    @Delete
+    void delete (Notes notes);
+}
